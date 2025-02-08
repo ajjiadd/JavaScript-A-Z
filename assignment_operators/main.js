1821dee5942f7ba1f2a1ef4d606d4f360762657e
ajjiad <ajjiad.cse@gmail.com>
2025-02-08 20:00:52 +0600

    complete more 3 chapter

---
+++ assignment_operators/main.js
@@ -0,0 +1,17 @@
+let text = "Hello";
+text += " World!";
+
+console.log(text);
+
+
+console.log("-----------------------")
+
+let num = 10;
+num -= 5;
+console.log(num)
+console.log("-----------------------")
+
+let x = 10;
+x *= 5;
+
+console.log(x)
